1cea3e591bf83a03974cd35d77158ed26da8189e
Javier Villanueva <javier@medialounge.co.uk>
2018-05-03 15:03:26 +0100

    Get application mode from project variables

--- Platformsh.php
+++ Platformsh.php
@@ -88,6 +88,8 @@ class Platformsh
     {
         $this->log("Start build.");
 
+        $_ENV['PLATFORM_VARIABLES'] = "eyJUSEVNRVMiOiBbIk1hZ2VudG8vbHVtYSIsICJNYWdlbnRvL2JhY2tlbmQiXSwgIkFQUExJQ0FUSU9OX01PREUiOiAicHJvZHVjdGlvbiIsICJBRE1JTl9QQVNTV09SRCI6ICJtZWQyMjJhIiwgIkFETUlOX1VTRVJOQU1FIjogIm1sZGV2IiwgIkxPQ0FMRVMiOiBbImVuX1VTIl19";
+
         $this->clearTemp();
 
         $this->compile();
@@ -117,7 +119,7 @@ class Platformsh
 
         $this->execute("rm -f app/etc/env.php");
 
-        $this->processMagentoMode();
+        $this->generateStaticContent();
     }
 
     /**
@@ -169,12 +171,6 @@ class Platformsh
         $this->adminEmail = isset($var["ADMIN_EMAIL"]) ? $var["ADMIN_EMAIL"] : "john@example.com";
         $this->adminPassword = isset($var["ADMIN_PASSWORD"]) ? $var["ADMIN_PASSWORD"] : "admin12";
         $this->adminUrl = isset($var["ADMIN_URL"]) ? $var["ADMIN_URL"] : "admin";
-
-        $this->desiredApplicationMode = isset($var["APPLICATION_MODE"]) ? $var["APPLICATION_MODE"] : false;
-        $this->desiredApplicationMode =
-            in_array($this->desiredApplicationMode, array(self::MAGENTO_DEVELOPER_MODE, self::MAGENTO_PRODUCTION_MODE))
-            ? $this->desiredApplicationMode
-            : false;
 
         $this->redisHost = $relationships['redis'][0]['host'];
         $this->redisScheme = $relationships['redis'][0]['scheme'];
@@ -408,7 +404,7 @@ class Platformsh
 
     protected function log($message)
     {
-        echo sprintf('[%s] %s', date("Y-m-d H:i:s"), $message) . PHP_EOL;
+        echo PHP_EOL . sprintf('[%s] %s', date("Y-m-d H:i:s"), $message) . PHP_EOL;
     }
 
     protected function execute($command)
@@ -516,12 +512,12 @@ class Platformsh
     /**
      * Based on variable APPLICATION_MODE. Production mode by default
      */
-    protected function processMagentoMode()
+    protected function generateStaticContent()
     {
-        if ($this->desiredApplicationMode === self::MAGENTO_PRODUCTION_MODE) {
-            $var = $this->getVariables();
+        $var = $this->getVariables();
 
-            $themesParam = "-t " . implode("-t ", $var["THEMES"]);
+        if ($var["APPLICATION_MODE"] === self::MAGENTO_PRODUCTION_MODE) {
+            $themesParam = "-t " . implode(" -t ", $var["THEMES"]);
             $localesParam = implode(" ", $var["LOCALES"]);
 
             $this->log("Generating static content for locales $localesParam.");
